1803902b7bac44888454e061e9151d87cf15a1ab
MichelleCharotti <michellecharotti@gmail.com>
2018-10-29 14:31:48 -0300

    PracticaRecuperatorio1erParcil

---
+++ Parciales/Rec1erParcialMascota/Charotti.Michelle.2A/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Charotti.Michelle._2A
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPrueba = new System.Windows.Forms.Button();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.rtbSalida = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnPrueba
+            // 
+            this.btnPrueba.Location = new System.Drawing.Point(25, 22);
+            this.btnPrueba.Name = "btnPrueba";
+            this.btnPrueba.Size = new System.Drawing.Size(411, 63);
+            this.btnPrueba.TabIndex = 0;
+            this.btnPrueba.Text = "Prueba Clases";
+            this.btnPrueba.UseVisualStyleBackColor = true;
+            this.btnPrueba.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Location = new System.Drawing.Point(25, 117);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(411, 65);
+            this.btnMostrar.TabIndex = 1;
+            this.btnMostrar.Text = "Mostrar Salida Por Pantalla";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // rtbSalida
+            // 
+            this.rtbSalida.Location = new System.Drawing.Point(12, 189);
+            this.rtbSalida.Name = "rtbSalida";
+            this.rtbSalida.Size = new System.Drawing.Size(436, 213);
+            this.rtbSalida.TabIndex = 3;
+            this.rtbSalida.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(460, 414);
+            this.Controls.Add(this.rtbSalida);
+            this.Controls.Add(this.btnMostrar);
+            this.Controls.Add(this.btnPrueba);
+            this.Name = "Form1";
+            this.Text = "RPP";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPrueba;
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.RichTextBox rtbSalida;
+    }
+}
+
---
+++ Parciales/Rec1erParcialMascota/Entidades/Caballo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Caballo:Animal
+    {
+        private string _nombre;
+        private static int _patas;
+
+        static Caballo()
+        {
+            Caballo._patas = 4;
+        }
+        public Caballo (string nombre,int velocidadMaxima):base(4,velocidadMaxima)
+        {
+            this._nombre = nombre;
+        }
+
+        public string MostrarCaballo()
+        {
+            return this.MostrarDatos()+ "\nnombre:" + this._nombre;
+        }
+
+        public static bool operator !=(Caballo c1,Caballo c2)
+        {
+            return !(c1 == c2);
+        }
+        public static bool operator ==(Caballo c1,Caballo c2)
+        {
+            return c1._nombre == c2._nombre;
+        }
+    }
+}
